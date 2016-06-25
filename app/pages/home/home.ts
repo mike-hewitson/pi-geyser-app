46a75a1ebe93a5f5e4b3603bd4a6262ff731fbeb
mikeyjcat <mike@hewitson.co.za>
2016-06-25 12:09:26 +0200

    feat: add Facebook Oauth

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,36 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, Platform} from 'ionic-angular';
+import {CordovaOauth, Facebook} from 'ng2-cordova-oauth/core';
+
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(private navController: NavController) {
-  
+
+  cordovaOauth;
+  platform;
+
+  static get parameters() {
+    return [[Platform]];
   }
+
+  constructor(platform) {
+    this.platform = platform;
+    this.cordovaOauth = new CordovaOauth(new Facebook({ clientId: "293408157664230", appScope: ["email"] }));
+  }
+
+  // constructor(private navController: NavController) {
+  // }
+
+  login() {
+    this.platform.ready().then(() => {
+      this.cordovaOauth.login().then((success) => {
+        alert(success.access_token);
+      }, (error) => {
+        alert(error);
+      });
+    });
+  }
+
 }
